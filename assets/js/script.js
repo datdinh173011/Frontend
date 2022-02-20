86d8dec625938056eeb0347f548201fe3d3c4b77
datdinh <datdinh201799@gmail.com>
2022-02-21 00:51:32 +0700

    Update Add new Device

--- assets/js/script.js
+++ assets/js/script.js
@@ -102,3 +102,13 @@ new Chart("myChart", {
         }
     }
 });
+
+// Get modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal , close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
